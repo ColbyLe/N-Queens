14cc07ea2349f3d031f2855848bc0e6286612622
ColbyLe <colbyle97@gmail.com>
2018-07-13 22:45:01 -0700

    added driver class

--- src/Node.java
+++ src/Node.java
@@ -17,25 +17,37 @@ public class Node {
 
         // check for # of pairs of attacking queens
         // only checks in one direction, so no pairs are repeated
+        //System.out.println("\nNew Node");
         for(int i=0; i<state.length; i++) {
             for(int j=i+1; j<state.length; j++) {
                 // check horizontal alignment
-                if((state[i] == state[j]) && horiz<2) horiz++;
+                if((state[i] == state[j])) {
+                    horiz++;
+                    //System.out.println(i + ": " + state[i] + " " + j + ": " + state[j]);
+                }
                 // if difference between vertical position = difference between horizontal position
                 // diagonal alignment
-                if((Math.abs(j-i) == Math.abs(state[j] - state[i])) && diag<2) diag++;
+                if((Math.abs(j-i) == Math.abs(state[j] - state[i]))) {
+                    diag++;
+                    //System.out.println(i + ": " + state[i] + " " + j + ": " + state[j]);
+                }
                 // each queen can only be aligned with one queen horizontally and one diagonally
-                if(horiz == 1 && diag == 1) break;
+                //if(horiz == 1 && diag == 1) break;
             }
         }
+        h = diag+horiz;
     }
 
     public int getH() {
         return h;
     }
 
+    public int[] getState() {
+        return state;
+    }
+
     // store last node's h in driver function
-    // if last node's h>=current h, or if h==0, stop search
+    // if last node's h<=current h, or if h==0, stop search
 
     // method to generate new nodes and select a successor based on steepest-ascent hill climbing
     public Node getNextSteepest() {
@@ -67,6 +79,25 @@ public class Node {
             newS[i] = s[i];
         }
         return newS;
+    }
+
+    public static void printState(Node n) {
+        System.out.print("[");
+        for(int i=0; i<n.state.length; i++) {
+            System.out.print(n.state[i]);
+            if(i!=n.state.length-1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void printBoard(Node n) {
+        for(int i=0; i<n.state.length; i++) {
+            for(int j=0; j<n.state.length; j++) {
+                if(i==n.state[j]) System.out.print("|X");
+                else System.out.print("| ");
+            }
+            System.out.println("|");
+        }
     }
 }
 
---
+++ src/Solver.java
@@ -0,0 +1,75 @@
+import java.util.Random;
+
+public class Solver {
+    public static void main(String[] args) {
+        System.out.println("22-Queen Solver");
+        int g = 100;
+        long sTime;
+        long[] tTime = new long[g];
+        boolean[] res = new boolean[g];
+        for(int i=0; i<g; i++) {
+            sTime = System.nanoTime();
+            System.out.println();
+            res[i] = steepest(randomize(22));
+            tTime[i] = timeElapsed(sTime);
+        }
+        System.out.println("Success rate: " + successRate(res) + "%");
+        System.out.println("Average running time: " + avgTime(tTime) + " ns");
+    }
+
+    public static boolean steepest(Node start) {
+        Node s = start;
+        int lastH;
+        int counter = 0;
+        do {
+            // System.out.println("Trial " + counter);
+            // counter++;
+            lastH = s.getH();
+            System.out.print(lastH);
+            s = s.getNextSteepest();
+            System.out.println(", " + s.getH());
+            if(s.getH() == 0) {
+                Node.printState(s);
+                Node.printBoard(s);
+                System.out.println("Solved!");
+                return true;
+            }
+        } while(lastH>s.getH());
+        Node.printState(s);
+        Node.printBoard(s);
+        return false;
+    }
+
+    public static Node randomize(int n) {
+        Random rd = new Random();
+        int[] s = new int[n];
+        for(int i =0; i<s.length; i++) {
+            s[i] = rd.nextInt(n);
+        }
+        Node ret = new Node(s);
+        //Node.printState(ret);
+        //System.out.println("h: " + ret.getH());
+        //Node.printBoard(ret);
+        return ret;
+    }
+
+    public static long timeElapsed(long start) {
+        return (System.nanoTime() - start);
+    }
+
+    public static float successRate(boolean[] b) {
+        int counter = 0;
+        for(boolean i : b) {
+            if(i) counter++;
+        }
+        return ((counter*100)/b.length);
+    }
+
+    public static long avgTime(long[] t) {
+        long acc = 0;
+        for(long i : t) {
+            acc+=i;
+        }
+        return (acc/t.length);
+    }
+}
